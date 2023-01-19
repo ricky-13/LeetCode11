1f90af90a18cc035a5cfbf8d39012d86178ec865
Ricky <ahujashaurya13@gmail.com>
2023-01-19 12:51:19 -0500

    adding changes for pascals triangle

--- Easy/pascals_triangle.java
+++ Easy/pascals_triangle.java
@@ -1,10 +1,29 @@
 package JustCode.Easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class pascals_triangle {
    public List<List<Integer>> generate(int numRows) {
+       List<List<Integer>> solList = new ArrayList<List<Integer>>();
+//       List l = [1];
+       solList.add(Arrays.asList(1));
+       solList.add(Arrays.asList(1,1));
 
+       for(int i = 3; i <= numRows; i++){
+           List<Integer> innerList = new ArrayList<Integer>();
+           innerList.add(1);
+
+           for(int j = 1; j <= i-2; j++){
+               int x = solList.get(i-1).get(j) + solList.get(j+1).get(j+1);
+           }
+
+
+       }
+
+       return null;
    }
-
+    
 }
