6acb24becfacaab1ea5a0beb1f313663fdb81837
Ricky <ahujashaurya13@gmail.com>
2023-01-19 14:39:32 -0500

    pascal triangle final

--- Easy/pascals_triangle.java
+++ Easy/pascals_triangle.java
@@ -6,24 +6,44 @@ import java.util.Collections;
 import java.util.List;
 
 public class pascals_triangle {
-   public List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> solList = new ArrayList<List<Integer>>();
-//       List l = [1];
+   public static List<List<Integer>> generate(int numRows) {
+       List<List<Integer>> solList = new ArrayList<>();
+
+       if(numRows == 1){
+           solList.add(Arrays.asList(1));
+           return solList;
+       }
+
+       if(numRows == 2){
+           solList.add(Arrays.asList(1));
+           solList.add(Arrays.asList(1,1));
+           return solList;
+       }
+
        solList.add(Arrays.asList(1));
        solList.add(Arrays.asList(1,1));
 
-       for(int i = 3; i <= numRows; i++){
+       for(int i = 2; i < numRows; i++){
+
            List<Integer> innerList = new ArrayList<Integer>();
            innerList.add(1);
 
-           for(int j = 1; j <= i-2; j++){
-               int x = solList.get(i-1).get(j) + solList.get(j+1).get(j+1);
+           for(int j=0; j < i-1; j++){
+               int x = solList.get(i-1).get(j) + solList.get(i-1).get(j+1);
+               innerList.add(x);
            }
 
-
+           innerList.add(1);
+           solList.add(innerList);
        }
-
-       return null;
+       return solList;
    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        List ans = new ArrayList();
+        ans = generate(3);
+        System.out.println(ans);
+    }
     
 }
