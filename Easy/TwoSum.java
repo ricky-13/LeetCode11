204dc13f7e9dfc48d4f8fb6ae026c4217fc26ab3
Ricky <ahujashaurya13@gmail.com>
2023-01-23 02:57:23 -0500

    adding code for TwoSum

---
+++ Easy/TwoSum.java
@@ -0,0 +1,45 @@
+package JustCode.Easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] solArray = new int[2];
+        HashMap<Integer, Integer> solMap = new HashMap();
+//        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            if(solMap.containsKey(target - nums[i])){
+                solArray[1] = i;
+                solArray[0] = solMap.get(target - nums[i]);
+                return solArray;
+            }
+
+            solMap.put(nums[i], i );
+        }
+
+
+//        for(int i = 0; i < nums.length; i++){
+//            for(int j = 0; j < nums.length; j++){
+//                if (nums[i] + nums[j] == target && i!=j){
+//                    solArray[0] = i;
+//                    solArray[1] = j;
+//                }
+//            }
+//        }
+
+        return solArray;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = new int[]{}
+
+        int[] arr = twoSum(new int[]{2,3,4}, 6);
+
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+    }
+}
